c99945d32f17eb35218e4214caa19409ab3bf91a
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2024-02-20 22:30:31 +1300

    Exception handling! Woohoo~

--- .php-utils/src/Validation.php
+++ .php-utils/src/Validation.php
@@ -2,10 +2,12 @@
 
 namespace GuySartorelli\DdevPhpUtils;
 
+use Symfony\Component\Console\Formatter\OutputFormatter;
 use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Throwable;
 
 final class Validation
 {
@@ -26,16 +28,66 @@ final class Validation
             InputOption::VALUE_NONE,
             'Provide verbose output including stack trace for errors'
         ));
+        $input = new ArgvInput();
 
-        $input = new ArgvInput(definition: $definition);
+        Output::init($input->hasParameterOption(['--verbose', '-v', '-vv', '-vvv'], true));
+
+        set_exception_handler([self::class, 'handleException']);
+
+        $input->bind($definition);
         $input->validate();
 
-        Output::init($input->getOption('verbose'));
-
-        // TODO:
-        // - Set up error handler to control error output
-        // - Provide cleaner error output if not verbose
-
         return $input;
+    }
+
+    /**
+     * Exception handler. Very reduced version of what symfony/console does in Application::renderThrowable().
+     */
+    public static function handleException(Throwable $e): void
+    {
+        if (Output::isVerbose()) {
+            // Padding
+            Output::getIO()->writeln('');
+            $preface = OutputFormatter::escape(sprintf(
+                'In %s line %s:',
+                basename($e->getFile()) ?: 'n/a',
+                $e->getLine() ?: 'n/a')
+            );
+            Output::getIO()->writeln(sprintf('<comment>%s</comment>', $preface));
+        }
+
+        Output::error(OutputFormatter::escape(trim($e->getMessage())));
+
+        if (Output::isVerbose()) {
+            Output::getIO()->writeln('<comment>Exception trace:</comment>');
+
+            // exception related properties
+            $trace = $e->getTrace();
+
+            array_unshift($trace, [
+                'function' => '',
+                'file' => $e->getFile() ?: 'n/a',
+                'line' => $e->getLine() ?: 'n/a',
+                'args' => [],
+            ]);
+
+            for ($i = 0, $count = \count($trace); $i < $count; ++$i) {
+                $class = $trace[$i]['class'] ?? '';
+                $type = $trace[$i]['type'] ?? '';
+                $function = $trace[$i]['function'] ?? '';
+                $file = $trace[$i]['file'] ?? 'n/a';
+                $line = $trace[$i]['line'] ?? 'n/a';
+
+                Output::getIO()->writeln(sprintf(
+                    ' %s%s at <info>%s:%s</info>',
+                    $class,
+                    $function ? $type.$function.'()' : '',
+                    $file,
+                    $line)
+                );
+                // Padding
+                Output::getIO()->writeln('');
+            }
+        }
     }
 }
